2df1f6d652d3615e032a4d13df87e7a841054452
madefu <madefu@DZ0400610madefu.local>
2021-08-09 19:47:34 +0800

    add

--- src/main/java/com/springdemo/controller/Hello.java
+++ src/main/java/com/springdemo/controller/Hello.java
@@ -1,5 +1,6 @@
 package com.springdemo.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,15 @@ public class Hello {
     @RequestMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @RequestMapping(value = "/path/{sn}")
+    public String test(@PathVariable String sn){
+        return sn;
+    }
+
+    @RequestMapping(value = "/param/req")
+    public String reParam(String name, Integer age){
+        return name + age;
     }
 }
