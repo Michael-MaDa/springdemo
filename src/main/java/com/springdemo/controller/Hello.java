5c77b776961c02b8e0575c2621e07d3bd6472ac5
madefu <madefu@DZ0400610madefu.local>
2021-08-10 14:27:47 +0800

    提交test

--- src/main/java/com/springdemo/controller/Hello.java
+++ src/main/java/com/springdemo/controller/Hello.java
@@ -1,6 +1,8 @@
 package com.springdemo.controller;
 
+import com.springdemo.model.Person;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,17 +10,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
 
     @RequestMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello";
     }
 
     @RequestMapping(value = "/path/{sn}")
-    public String test(@PathVariable String sn){
+    public String test(@PathVariable String sn) {
         return sn;
     }
 
     @RequestMapping(value = "/param/req")
-    public String reParam(String name, Integer age){
+    public String reParam(String name, Integer age) {
         return name + age;
     }
+
+    @RequestMapping(value = "/param/model")
+    public String modelParam(Person person) {
+        return person.toString();
+    }
+
+    @RequestMapping(value = "/param/json")
+    public String josnParam(@RequestBody Person person) {
+        return person.toString();
+    }
+
 }
